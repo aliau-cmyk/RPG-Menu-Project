f2cd7b93ccb9f64e02419e6f905a405a93e6b9a0
aliau-cmyk <53334519+aliau-cmyk@users.noreply.github.com>
2021-04-11 02:09:08 -0700

    Add files via upload

--- RPG Project/Scenes/Scene2.js
+++ RPG Project/Scenes/Scene2.js
@@ -4,6 +4,15 @@ class Scene2 extends Phaser.Scene {
     }
 
     create(){
+        // sets class variable for image
+        // this.add.image(0,0,"background");
+        // position: the x and y coordinates
+        // key: the key of the image in preload
+        this.backgroundtest = this.add.image(0,0, "backgroundtest");
+
+        // changes pivot of image to top left instead of center
+        this.backgroundtest.setOrigin(0,0);
+
         this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
     }
 }
--- RPG Project/game.js
+++ RPG Project/game.js
@@ -1,10 +1,10 @@
 // object var config that can be passed into game
 var config = {
-    with: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     backgroundColor: 0x000000,
     // scenes are put into array for config
-    scene: [Scene1, Scene2]
+    scene: [StartScene, Scene2]
 }
 
 // create an instance of the game
