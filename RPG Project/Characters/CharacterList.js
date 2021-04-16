95e9e133679871517fe0c2edcb905bf08cbe23eb
aliau-cmyk <53334519+aliau-cmyk@users.noreply.github.com>
2021-04-15 19:17:21 -0700

    Added Characters Directory, Testing out PlayerData

---
+++ RPG Project/Characters/CharacterList.js
@@ -0,0 +1,7 @@
+import {CharacterTemplate} from './CharacterTemplate';
+import {basicAttack, throwDust} from './AttackList';
+
+
+// TESTING TO SEE IF IMPORTS ACTUALLY WORK
+console.log("hi");
+console.log(basicAttack.attackName);
---
+++ RPG Project/Characters/CharacterTemplate.js
@@ -0,0 +1,14 @@
+//template class for characters
+class CharacterTemplate{
+
+    constructor (name)
+    {
+        this.name = name;
+        this.maxHealth;
+        this.currentHealth;
+        this.moveset = [];
+    }
+
+}
+
+export {CharacterTemplate};
