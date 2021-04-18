aa7a4eb2396a1e7958aabf94484e5d276864362c
Mabel-Mei <79951129+Mabel-Mei@users.noreply.github.com>
2021-04-19 01:32:43 +0800

    Add files via upload

--- RPG Project/game.js
+++ RPG Project/game.js
@@ -1,10 +1,15 @@
 // object var config that can be passed into game
 var config = {
-    width: 1920,
-    height: 1080,
+    type: Phaser.CANVAS,
     backgroundColor: 0x000000,
+    scale: {
+        mode: Phaser.Scale.FIT, //make our canvas fit in the center of the website
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
     // scenes are put into array for config
-    scene: [StartScene, DialogueScene,CharacterSelection,ActionScene]
+    scene: [StartScene, DialogueScene,CharacterSelection,ActionScene,UIScene]
 }
 
 // create an instance of the game
